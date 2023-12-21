b023d1c3b933d4dac49d9b101c2ea3f8599a4956
jessicavsampaio <8433012+jessicavsampaio@users.noreply.github.com>
2023-12-21 13:26:29 -0300

    Organizando select por ordem alfabética

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -31,24 +31,24 @@ export default function SearchBar({ pokemonFilter }) {
             {searchType === 'tipo' && (
                 <select required value={searchValue} onChange={handleSelectChange}>
                     <option value="">Selecione uma opção</option>
-                    <option value="normal">Normal</option>
-                    <option value="fire">Fire</option>
-                    <option value="water">Water</option>
-                    <option value="electric">Electric</option>
-                    <option value="grass">Grass</option>
-                    <option value="ice">Ice</option>
-                    <option value="fighting">Fighting</option>
-                    <option value="poison">Poison</option>
-                    <option value="ground">Ground</option>
-                    <option value="flying">Flying</option>
-                    <option value="psychic">Psychic</option>
                     <option value="bug">Bug</option>
-                    <option value="rock">Rock</option>
-                    <option value="ghost">Ghost</option>
-                    <option value="dragon">Dragon</option>
                     <option value="dark">Dark</option>
-                    <option value="steel">Steel</option>
+                    <option value="dragon">Dragon</option>
+                    <option value="electric">Electric</option>
                     <option value="fairy">Fairy</option>
+                    <option value="fighting">Fighting</option>
+                    <option value="fire">Fire</option>
+                    <option value="flying">Flying</option>
+                    <option value="ghost">Ghost</option>
+                    <option value="grass">Grass</option>
+                    <option value="ground">Ground</option>
+                    <option value="ice">Ice</option>
+                    <option value="normal">Normal</option>
+                    <option value="poison">Poison</option>
+                    <option value="psychic">Psychic</option>
+                    <option value="rock">Rock</option>
+                    <option value="steel">Steel</option>
+                    <option value="water">Water</option>
                 </select>
             )}
             {(searchType === 'nome' || searchType === 'nome-tipo') && (
